ba5106af5ae60108b4485a122ac2e74b85df1587
Nnamdi <nnamdijr@gmail.com>
2015-08-03 17:09:55 -0700

    Linguistic Features Update

--- LinguisticFeats.py
+++ LinguisticFeats.py
@@ -2,7 +2,10 @@ __author__ = 'Nnamdi'
 
 import time
 start_time = time.time()
-import os, codecs
+import codecs
+import os
+import string
+import subprocess
 import numpy
 from scipy.sparse import hstack, csr_matrix
 from nltk import sent_tokenize, word_tokenize
@@ -21,45 +24,109 @@ labels_vector = data[4]
 keywords_vector = data[2]
 
 #Empty matrix for load columns
-loader_matrix = numpy.empty([rows, 8])
+loader_matrix = numpy.empty([1, 7])
 
-def wordCount(fname):
-    count = os.system('wc -w %s' % fname)
+def word_count(fname):
+    count = subprocess.check_output(('wc -w %s' % fname), shell=True)
+    count = int(count.strip(' temp.txt\n'))
     return count
 
-def sentPerStatus(fname):
+def sent_per_status(fname):
     sent_count = []
-    for line in fname:
-        sent_count.append(len(sent_tokenize(line)))
+    with codecs.open(fname, encoding='utf-8') as temp:
+        for line in temp:
+            sent_count.append(len(sent_tokenize(line)))
     avg = sum(sent_count)/float(len(sent_count))
+    temp.close()
     return avg
 
-def wordsPerSent(fname):
+def words_per_sent(fname):
     word_count = []
-    for line in fname:
-        for sent in sent_tokenize(line):
-            word_count.append(len(word_tokenize(sent)))
+    with codecs.open(fname, encoding='utf-8') as temp:
+        for line in temp:
+            for sent in sent_tokenize(line):
+                word_count.append(len(word_tokenize(sent)))
     avg = sum(word_count)/float(len(word_count))
+    temp.close()
     return avg
 
-def punctCount(fname):
-    return 1
+def punct_count(fname):
+    count = 0
+    temp = codecs.open(fname, encoding='utf-8')
+    text = temp.read()
+    text = word_tokenize(text)
+    for token in text:
+        if token in string.punctuation:
+            count += 1
+        continue
+    temp.close()
+    return count
 
 def lexical_diversity(fname):
-    text = fname.read()
-    text = word_tokenize(text)
+    temp = codecs.open(fname, encoding='utf-8')
+    text = temp.read()
+    text = word_tokenize(text.lower())
     diversity = len(text) / float(len(set(text)))
+    temp.close()
     return diversity
 
 def emoticons(fname):
-    return 1
+    count = 0
+    with open('emoticons.txt','r') as f:
+        emtcns = [l.strip() for l in f]
+    with codecs.open(fname, encoding='utf-8') as temp:
+        for line in temp:
+            for icon in emtcns:
+                if icon in line:
+                    count += 1
+                continue
+    temp.close()
+    f.close()
+    return count
 
 def acronyms(fname):
-    return 1
+    count = 0
+    temp = codecs.open(fname, encoding='utf-8')
+    text = temp.read()
+    text = word_tokenize(text.lower())
+    with open('acronyms.txt','r') as f:
+        acrnms = [l.strip().lower() for l in f]
+    for token in text:
+        if token in acrnms:
+            count += 1
+        continue
+    temp.close()
+    f.close()
+    return count
 
 def profanities(fname):
-    return 1
+    count = 0
+    temp = codecs.open(fname, encoding='utf-8')
+    text = temp.read()
+    text = word_tokenize(text.lower())
+    with open('profanities.txt','r') as f:
+        prfnts = [l.strip() for l in f]
+    for token in text:
+        if token in prfnts:
+            count += 1
+        continue
+    temp.close()
+    f.close()
+    return count
 
-for user in users_vector:
+for user in users_vector[:6]:
     os.system('grep -i "{0}{1}" /analytic_store/tmpdata/smoking_1_noUser0.txt > temp.txt'.format('smoking_1_', str(user)))
-    fname = codecs.open('/analytic_store/nnamdi/temp.txt', encoding='utf-8')
+    temp_file = 'temp.txt'
+    user_array = numpy.array([word_count(temp_file), sent_per_status(temp_file), words_per_sent(temp_file),
+                              punct_count(temp_file), lexical_diversity(temp_file), acronyms(temp_file),
+                              profanities(temp_file)])
+
+    loader_matrix = numpy.vstack((loader_matrix, user_array))
+    print("--- %s seconds ---" % (time.time() - start_time))
+
+loader_matrix = csr_matrix(loader_matrix)[1:,:] #Convert loader_matrix to csr matrix and remove first row
+combined_matrix = hstack([posts_matrix, loader_matrix],format="csr") #combine loader_matrix with posts_matrix
+
+print "Loader Matrix Shape:", loader_matrix.shape
+print "Posts Matrix Shape:", csr_matrix.get_shape(posts_matrix)
+print "Combined Matrix Shape:", csr_matrix.get_shape(combined_matrix)
